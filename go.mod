b38a30d3ea47714b77714dc5cc88c860e14ff90b
sakshamagrawal07 <agsaksham07@gmail.com>
2026-06-13 14:25:05 +0530

    simple tcp echo server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sakshamsharma/deris-v2
+
+go 1.24.4
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/sakshamsharma/deris-v2/server"
+	"github.com/sakshamsharma/deris-v2/config"
+)
+
+func setupFlags() {
+	flag.StringVar(&config.Host, "host", "0.0.0.0", "Host for the deris server")
+	flag.IntVar(&config.Port, "port", 7379, "Port for the deris server")
+	flag.Parse()
+}
+
+func main() {
+	setupFlags()
+	log.Println("Deris server rolling...")
+	server.RunSyncTCPServer();
+}
